3b5df54fe10907d0149fb30c3523f128a08abb10
Andres Solano S <andres.solano@gmail.com>
2016-07-27 11:05:58 -0600

    update bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,6 +19,21 @@ chown vagrant:vagrant /home/vagrant/tmp
 echo "added github.com -oStrictHostKeyChecking=no"
 ssh -oStrictHostKeyChecking=no -T git@github.com
 
+
+#adding virtualhosts
+echo "adding virtualhosts local.naturallycurly.com and local.backend.naturallycurly.com"
+rm -r /etc/apache2/sites-available/
+mv /var/www/sites-available /etc/apache2/
+rm /etc/apache2/sites-enabled/*
+cd /etc/apache2/sites-enabled/
+ln -s ../sites-available/default-ssl.conf .
+ln -s ../sites-available/local.naturallycurly.conf .
+ln -s ../sites-available/local.backend.naturallycurly.conf .
+ln -s ../sites-available/local.api.naturallycurly.conf .
+service apache2 restart
+
+
 echo "remember added this virtualhosts to /etc/hosts like this:"
 echo "192.168.77.17	local.naturallycurly.com"
 echo "192.168.77.17	local.backend.naturallycurly.com"
+echo "192.168.77.17	local.api.naturallycurly.com"
